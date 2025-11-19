422a77dd745a2db663c352a1ffdd8b2694556160
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2025-11-19 23:52:11 +0100

    NS Session App v1.1.17: Update Version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.1.16";
+const APP_VERSION = "1.1.17";
 const CACHE_VERSION = APP_VERSION.replaceAll(".", '');
 const CACHE_PREFIX = "ns-app-cache-";
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.1.16";
-export const DB_VERSION = "2025.11.12.1";
+export const APP_VERSION = "1.1.17";
+export const DB_VERSION = "2025.11.19.1";
